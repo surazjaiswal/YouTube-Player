c3cf8ecd88dec16bfc80ad9fe5c7db97931a3383
Suraj Jaiswal <surazjaiswal@gmail.com>
2022-05-18 01:20:19 +0530

    Screen 3 Compeleted

--- app/src/main/java/com/practice/ytplayer/Form.java
+++ app/src/main/java/com/practice/ytplayer/Form.java
@@ -1,8 +1,18 @@
 package com.practice.ytplayer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,14 +21,19 @@ import android.widget.Toast;
 
 public class Form extends AppCompatActivity {
 
+    public static final int CAMERA_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+
     EditText txt_name,txt_email;
     ImageButton imageButton;
 
     Button btn_submit;
-
     String name,email;
+    Bitmap bitmap;
 
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,13 @@ public class Form extends AppCompatActivity {
         txt_email = findViewById(R.id.txt_email);
         btn_submit = findViewById(R.id.btn_submit);
 
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraPermissions();
+            }
+        });
+
 
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,18 +59,61 @@ public class Form extends AppCompatActivity {
                 name = txt_name.getText().toString().trim();
                 email = txt_email.getText().toString().trim();
 
-                if(!email.matches(emailPattern)){
+                if(name.isEmpty() || email.isEmpty()){
+                    Toast.makeText(Form.this, "Please Provide All the Details", Toast.LENGTH_SHORT).show();
+                }
+                else if(!email.matches(emailPattern)){
                     Toast.makeText(Form.this, "Invalid Email", Toast.LENGTH_SHORT).show();
-                    return;
-                }else{
+                }
+                else if(bitmap == null){
+                    Toast.makeText(Form.this, "No Image Provided", Toast.LENGTH_SHORT).show();
+                }
+                else{
                     // forward the data to next page
-                    Toast.makeText(Form.this, "Valid Email", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(Form.this, "Valid Email", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Form.this,Profile.class);
+                    intent.putExtra("name",name);
+                    intent.putExtra("email",email);
+                    intent.putExtra("img",bitmap);
+
+                    startActivity(intent);
                 }
             }
         });
+    }
+    void cameraPermissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA}, CAMERA_CODE);
+        }else{
+            openCamera();
+        }
 
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CAMERA_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openCamera();
+            }else{
+            Toast.makeText(this, "Camera Permission Required", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
+    private void openCamera() {
+        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera_intent, CAMERA_REQUEST_CODE);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_REQUEST_CODE){
+            bitmap = (Bitmap) data.getExtras().get("data");
+            imageButton.setImageBitmap(bitmap);
+            Toast.makeText(this, "Image Received", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/practice/ytplayer/Profile.java
+++ app/src/main/java/com/practice/ytplayer/Profile.java
@@ -2,13 +2,36 @@ package com.practice.ytplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView tv_name, tv_email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageView = findViewById(R.id.imgView);
+        tv_name = findViewById(R.id.tv_name);
+        tv_email = findViewById(R.id.tv_email);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String email = intent.getStringExtra("email");
+        Bitmap bitmap = intent.getParcelableExtra("img");
+
+        tv_name.setText(name);
+        tv_email.setText(email);
+        imageView.setImageBitmap(bitmap);
+
+
     }
 }
